6208d0a3ee3a92467fb541b0543059855af3d78d
RAGL90 <ricgarlue@gmail.com>
2024-09-27 11:43:07 +0200

    Config de dev

--- next.config.mjs
+++ next.config.mjs
@@ -12,8 +12,8 @@ const nextConfig = {
         // protocol: "http", //Aqui en local
         protocol: "https",
         // hostname: "192.168.2.2", //Dónde "localhost" irá la IP  o dominio real
-        // hostname: "localhost",
-        hostname: "spotmypet.es",
+        hostname: "localhost",
+        // hostname: "spotmypet.es",
       },
     ],
   },
